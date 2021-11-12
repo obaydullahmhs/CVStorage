2bb84826ed178ddb443791b4a7b4aac266e2fb8d
arnab_sheikh <arnab.sheikh007@gmail.com>
2021-11-13 01:37:29 +0600

    Add project files.

---
+++ CVStorage/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using CVStorage.Areas.Identity.Data;
+using CVStorage.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CVStorage.Areas.Identity.IdentityHostingStartup))]
+namespace CVStorage.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<CVStorageDbContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("CVStorageDbContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
+                .AddRoles<IdentityRole>()
+                .AddEntityFrameworkStores<CVStorageDbContext>();
+                
+            });
+        }
+    }
+}
